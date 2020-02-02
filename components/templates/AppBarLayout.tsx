1b02262ea5a72a00e132e43d331b4bae00df414a
mrkwon <erased11@gmail.com>
2020-02-03 00:12:06 +0900

    [#1] feat - AppBarLayout, 페이지 기본 레이아웃 구현

---
+++ components/templates/AppBarLayout.tsx
@@ -0,0 +1,39 @@
+import {Button, Form, FormControl, Nav, Navbar} from "react-bootstrap";
+import * as React from "react";
+import {CSSProperties} from "react";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+interface IProps {
+    children: object;
+}
+
+const style: CSSProperties = {
+    height: '100vh',
+    width: '100vw',
+};
+
+const AppBarLayout = ({children}: IProps) => {
+    return (
+        <div style={style}>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="/">테코브러리 어드민</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">대시보드</Nav.Link>
+                    <Nav.Link href="/users">회원 관리</Nav.Link>
+                    <Nav.Link href="/books">장서 관리</Nav.Link>
+                    <Nav.Link href="/wishes">희망도서 관리</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="검색" className="mr-sm-2"/>
+                    <Button variant="outline-info">검색</Button>
+                </Form>
+            </Navbar>
+            <div>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default AppBarLayout;
