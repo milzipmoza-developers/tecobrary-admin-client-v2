67215c8086d6faef2a069ae6154aeb569386108c
mrkwon <erased11@gmail.com>
2020-02-05 21:27:27 +0900

    [#1] feat - 제목 링크 추가

--- components/organisms/WishBookSummaryCard.tsx
+++ components/organisms/WishBookSummaryCard.tsx
@@ -2,6 +2,7 @@ import {Card, ListGroup} from "react-bootstrap";
 import WishBookSummaryElement from "../molecules/WishBookSummaryElement";
 import * as React from "react";
 import {IWishBook} from "../../pages/dashboard";
+import {useRouter} from "next/router";
 
 interface IProps {
     wishBooks: IWishBook[];
@@ -19,11 +20,19 @@ const WishBookSummaryCardBody = ({wishBooks}: IProps) => (
     </div>
 );
 
-const WishBookSummaryCard = ({wishBooks}: IProps) => (
-    <Card style={{width: '100%', maxHeight: '435px'}}>
-        <Card.Header as="h5">희망도서</Card.Header>
-        <WishBookSummaryCardBody wishBooks={wishBooks}/>
-    </Card>
-);
+const WishBookSummaryCard = ({wishBooks}: IProps) => {
+    const router = useRouter();
+
+    const titleClickHandler = () => {
+        router.push('/wishes')
+    };
+
+    return (
+        <Card style={{width: '100%', maxHeight: '435px'}}>
+            <Card.Header as="h5" onClick={titleClickHandler} style={{cursor: 'pointer'}}>희망도서</Card.Header>
+            <WishBookSummaryCardBody wishBooks={wishBooks}/>
+        </Card>
+    );
+};
 
 export default WishBookSummaryCard;
